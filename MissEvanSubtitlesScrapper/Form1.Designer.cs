cc5392069fdbcce15977c9cc9c18a1edda8a8f5a
Shashok Natalia <n.shashok@g.nsu.ru>
2020-06-23 23:59:52 +0700

    Add files via upload

---
+++ MissEvanSubtitlesScrapper/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MissEvanSubtitlesScrapper
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectID = new System.Windows.Forms.Label();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.resultingText = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectID
+            // 
+            this.selectID.AutoSize = true;
+            this.selectID.Location = new System.Drawing.Point(16, 4);
+            this.selectID.Name = "selectID";
+            this.selectID.Size = new System.Drawing.Size(67, 17);
+            this.selectID.TabIndex = 0;
+            this.selectID.Text = "ID трека:";
+            this.selectID.Click += new System.EventHandler(this.selectID_Click);
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(15, 27);
+            this.numericUpDown.Maximum = new decimal(new int[] {
+            1410065408,
+            2,
+            0,
+            0});
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown.TabIndex = 1;
+            this.numericUpDown.ValueChanged += new System.EventHandler(this.numericUpDown_ValueChanged);
+            // 
+            // resultingText
+            // 
+            this.resultingText.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.resultingText.Location = new System.Drawing.Point(15, 94);
+            this.resultingText.Multiline = true;
+            this.resultingText.Name = "resultingText";
+            this.resultingText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.resultingText.Size = new System.Drawing.Size(750, 310);
+            this.resultingText.TabIndex = 2;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(15, 55);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(120, 33);
+            this.start.TabIndex = 3;
+            this.start.Text = "Обработать";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.resultingText);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.selectID);
+            this.Name = "Form";
+            this.Text = "Субтитры с MaoerFM";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label selectID;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.TextBox resultingText;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ MissEvanSubtitlesScrapper/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace MissEvanSubtitlesScrapper
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+            InitializeComponent();
+        }
+
+        private void selectID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            var link = "https://www.missevan.com/sound/getdm?soundid=" + this.numericUpDown.Value;
+            this.PostRequest(link);
+        }
+
+        private void PostRequest(string link)
+        {
+            WebRequest request = WebRequest.Create(link);
+            WebResponse response = request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string responseText = reader.ReadToEnd();
+            string result = "";
+            var dict = new List<Tuple<float, string>>();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(responseText);
+            XmlNodeList nodes = xmlDoc.GetElementsByTagName("d");
+            foreach (XmlNode node in nodes)
+            {
+                string p = node.Attributes["p"].Value;
+                string[] numbers = p.Split(',');
+                if (numbers[5] != "195") continue;
+                float time = float.Parse(numbers[0]);
+                dict.Add(Tuple.Create(time, node.InnerText));
+            }
+
+            dict.Sort((x, y) => x.Item1.CompareTo(y.Item1));
+
+            foreach(var tuple in dict)
+            {
+                result += tuple.Item1 + "\r\n\t" + tuple.Item2 + "\r\n\r\n";
+            }
+
+            this.resultingText.Text = "";
+            this.resultingText.Text = result;
+        }
+    }
+}
